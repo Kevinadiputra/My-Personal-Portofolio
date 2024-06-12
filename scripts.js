f82488451871f091420322b58806cc1fd0f0b3b3
kevinadiputra66 <kevinadiputra66@gmail.com>
2024-06-12 00:19:40 +0000

    commit

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var message = document.getElementById('message').value;
+document.querySelector("form").addEventListener("submit", function (event) {
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	var message = document.getElementById("message").value;
 
-    if (!name || !email || !message) {
-        alert('Harap isi semua bidang.');
-        event.preventDefault();
-    }
+	if (!name || !email || !message) {
+		alert("Harap isi semua bidang.");
+		event.preventDefault();
+	}
 });
