4a1eaa5c908cdb0f34bca257b91fd64c9a4a9cd9
kevinadiputra66 <kevinadiputra66@gmail.com>
2024-07-25 12:55:03 +0000

    commit

--- scripts.js
+++ scripts.js
@@ -28,36 +28,13 @@ document.addEventListener("DOMContentLoaded", function () {
 	sections.forEach((section) => {
 		observer.observe(section);
 	});
-});
 
-document.addEventListener("DOMContentLoaded", function () {
 	document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 		anchor.addEventListener("click", function (e) {
 			e.preventDefault();
-
 			document.querySelector(this.getAttribute("href")).scrollIntoView({
 				behavior: "smooth",
 			});
 		});
-	});
-
-	const sections = document.querySelectorAll(".section");
-	const observer = new IntersectionObserver(
-		(entries) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					entry.target.classList.add("visible");
-				} else {
-					entry.target.classList.remove("visible");
-				}
-			});
-		},
-		{
-			threshold: 0.1,
-		}
-	);
-
-	sections.forEach((section) => {
-		observer.observe(section);
 	});
 });
