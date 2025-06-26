86805fb5b689b63ce15b8b5cc10c793b14b4c956
Kevin Adiputra Mahesa <134620675+Kevinadiputra@users.noreply.github.com>
2025-06-26 18:44:54 +0700

    commit

---
+++ portofolio-website/src/app/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <section><h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>Hello, I'm Kevin</h1>
+            <p className='text-[#ADB7BE]'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic in unde dicta temporibus est saepe eum vitae, quo nulla aspernatur consectetur pariatur voluptatibus accusamus quisquam. Velit autem mollitia quisquam.
+            </p>
+        </section>
+    )
+}
+
+export default HeroSection
