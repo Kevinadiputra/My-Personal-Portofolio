14ce8d35909499caa44146a369aed2ccf0a4f8fd
Kevin Adiputra Mahesa <134620675+Kevinadiputra@users.noreply.github.com>
2025-06-27 08:42:37 +0700

    edit hero image

--- portofolio-website/src/app/components/HeroSection.jsx
+++ portofolio-website/src/app/components/HeroSection.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import Image from 'next/image'
 
 const HeroSection = () => {
     return (
-        <section><h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>Hello, I'm Kevin</h1>
-            <p className='text-[#ADB7BE]'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic in unde dicta temporibus est saepe eum vitae, quo nulla aspernatur consectetur pariatur voluptatibus accusamus quisquam. Velit autem mollitia quisquam.
-            </p>
+        <section>
+            <div className='grid grid-cols-1 lg:grid-cols-12'>
+                <div className='col-span-7 place-self-center'>
+                    <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
+                        Hello, I'm Kevin
+                    </h1>
+                    <p className='text-[#ADB7BE] text-lg lg:text-xl'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic in unde dicta temporibus est saepe eum vitae, quo nulla aspernatur consectetur pariatur voluptatibus accusamus quisquam. Velit autem mollitia quisquam.
+                    </p>
+                </div>
+            </div>
+            <div className='col-span-5'>
+                <Image
+                    src='/images/hero-image.jpg'
+                    alt='hero image'
+                    width={300}
+                    height={300}
+                />
+            </div>
         </section>
     )
 }
