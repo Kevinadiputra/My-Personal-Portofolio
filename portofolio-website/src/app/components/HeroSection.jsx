7cb071b1d85fe6aad020c0e7daea22162b02a2e4
Kevin Adiputra Mahesa <134620675+Kevinadiputra@users.noreply.github.com>
2025-06-27 16:17:25 +0700

    change description and add button

--- portofolio-website/src/app/components/HeroSection.jsx
+++ portofolio-website/src/app/components/HeroSection.jsx
@@ -4,29 +4,38 @@ import Image from 'next/image'
 const HeroSection = () => {
   return (
     <section>
-      <div className='grid grid-cols-1 lg:grid-cols-12'>
-        <div className='col-span-7 place-self-center'>
-          <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
+      <div className='grid grid-cols-1 sm:grid-cols-12'>
+        <div className='col-span-7 place-self-center text-center sm:text-left'>
+          <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
             Hello, I'm Kevin
           </h1>
-          <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic in unde dicta temporibus est saepe eum vitae, quo nulla aspernatur consectetur pariatur voluptatibus accusamus quisquam. Velit autem mollitia quisquam.
+          <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
+            I am a passionate Data Scientist, Machine Learning Engineer, and Data
+            Analyst with experience in building data-driven solutions, developing
+            predictive models, and uncovering actionable insights from complex
+            datasets. My portfolio showcases projects in data analysis, machine learning,
+            and end-to-end data pipelines, demonstrating my ability to turn raw data into
+            valuable business outcomes.
           </p>
           <div>
-            <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Contact me</button>
-            <button className='px-6 py-3 rounded-full'>Download CV</button>
+            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>
+              Contact me
+            </button>
+            <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>
+              Download CV
+            </button>
           </div>
         </div>
-      </div>
-      <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
-        <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-          <Image
-            src='/images/hero-image.png'
-            alt='hero image'
-            className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-            width={300}
-            height={300}
-          />
+        <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src='/images/hero-image.png'
+              alt='hero image'
+              className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+              width={300}
+              height={300}
+            />
+          </div>
         </div>
       </div>
     </section>
